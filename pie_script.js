ddaee8fb2aa6b38f4025739ab890c7f5c070c36a
Savio Abuga <savioabuga@gmail.com>
2014-12-07 15:52:55 +0300

    improved the pir chart

--- pie_script.js
+++ pie_script.js
@@ -2,14 +2,27 @@ var width = 400,
     height = 400,
     radius = 200;
 
-var colors = d3.scale.category20c();
+var colors = d3.scale.ordinal()
+    .range(['#595AB7','#A57706','#D11C24','#C61C6F','#BD3613','#2176C7','#259286','#738A05'])
 
 var piedata = [
     {label: "Barrot",
      value: 50},
     {label: "Gerald",
-     value: 90},
+     value: 50},
     {label: "Jennifer",
+     value: 50},
+     {label: "Lorenzo",
+     value: 50},
+    {label: "Alejandro",
+     value: 50},
+    {label: "Miguel",
+     value: 50},
+     {label: "John",
+     value: 50},
+    {label: "Michael",
+     value: 50},
+    {label: "Peter",
      value: 50}
 ]
 
@@ -27,8 +40,28 @@ var myChart = d3.select('#chart').append('svg')
     .append('g')
     .attr('transform', 'translate('+ (width - radius) + ', ' + (height-radius)+' )' )
     .selectAll('path').data(pie(piedata))
-    .enter().append('path')
-        .attr('fill', function (d, i) {
-            return colors(i);
-        })
-        .attr('d', arc)
+    .enter().append('g')
+        .attr('class', 'slice')
+
+
+var slices = d3.selectAll('g.slice')
+    .append('path')
+    .attr('fill', function (d, i) {
+        return colors(i);
+    })
+    .attr('d', arc)
+
+var text = d3.selectAll('g.slice')
+    .append('text')
+    .text(function(d, i){
+        return d.data.label;
+    })
+    .attr('text-anchor', 'middle')
+    .attr('fill', 'white')
+    .attr('transform', function(d){
+        d.innerRadius = 0;
+        d.outerRadius = radius;
+        return 'translate(' + arc.centroid(d) + ')'
+    })
+
+
